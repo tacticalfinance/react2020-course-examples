0f5f9ddcdb10052b1800ec0e18c189f7e23d2043
Ruby Salton <ruby@tornadofx.co.il>
2020-06-16 15:08:52 +0300

    lab 11: task 04: div with background color - show the initial bg color in the input field

--- 11-lab/src/div-color.js
+++ 11-lab/src/div-color.js
@@ -3,22 +3,22 @@ import { useState } from 'react';
 
 export default function DivColor(props) {
   const { defaultColor } = props;
-  const [bgColor, setBgColor] = useState(`#${defaultColor}`);
+  const [bgColor, setBgColor] = useState(defaultColor);
 
   const divStyle = {
     width: '100px',
     marginLeft: '10px',
-    backgroundColor: bgColor,
+    backgroundColor: `#${bgColor}`,
   }
 
   function onTextChanged(e) {
-    setBgColor(`#${e.target.value}`);
+    setBgColor(e.target.value);
   }
 
   return (
     <div style={{ display: 'flex' }}>
       <span>Fill in a color:</span>
-      <input type="text" style={{ marginLeft: '10px' }} onChange={onTextChanged} />
+      <input type="text" style={{ marginLeft: '10px' }} value={bgColor} onChange={onTextChanged} />
       <div style={divStyle}></div>
     </div>
   );
