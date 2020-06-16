1baad36a28afda96bbca519b3cd6adf70791ec5f
Ruby Salton <ruby@tornadofx.co.il>
2020-06-16 15:06:00 +0300

    lab 11: task 04: div with background color

---
+++ 11-lab/src/div-color.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useState } from 'react';
+
+export default function DivColor(props) {
+  const { defaultColor } = props;
+  const [bgColor, setBgColor] = useState(`#${defaultColor}`);
+
+  const divStyle = {
+    width: '100px',
+    marginLeft: '10px',
+    backgroundColor: bgColor,
+  }
+
+  function onTextChanged(e) {
+    setBgColor(`#${e.target.value}`);
+  }
+
+  return (
+    <div style={{ display: 'flex' }}>
+      <span>Fill in a color:</span>
+      <input type="text" style={{ marginLeft: '10px' }} onChange={onTextChanged} />
+      <div style={divStyle}></div>
+    </div>
+  );
+}
--- 11-lab/src/main.js
+++ 11-lab/src/main.js
@@ -3,19 +3,23 @@ import ReactDOM from 'react-dom';
 import MultiInput from './multi-input';
 import TimeParser from './time-parser';
 import GuessTheNumber from './guess-the-number';
+import DivColor from './div-color';
 
 const App = () => {
 
   return (
     <div>
-      <h1>01. Multi Input</h1>
-      <MultiInput />
+      <h1>04. Div Color</h1>
+      <DivColor defaultColor="ff9900" />
+
+      <h1>03. Guess The Number</h1>
+      <GuessTheNumber />
 
       <h1>02. Time Parser</h1>
       <TimeParser />
-      
-      <h1>03. Guess The Number</h1>
-      <GuessTheNumber />
+
+      <h1>01. Multi Input</h1>
+      <MultiInput />
     </div>
   )
 };
