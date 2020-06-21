b387996d3b24f2cc87f1275b2cfa1515e13c7820
Ruby Salton <ruby@tornadofx.co.il>
2020-06-21 10:09:39 +0300

    reset city selection using key (learned on lesson 17)

--- 15-select-country-and-city/after/src/select_country_and_city.js
+++ 15-select-country-and-city/after/src/select_country_and_city.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { useState } from 'react';
+import { unique } from 'jquery';
 
 const SelectCountry = (props) => {
   const { selectedCountry, setSelectedCountry, countries } = props;
 
   return (
-  <select value={selectedCountry} onChange={(e) => setSelectedCountry(e.target.value)}>
-    <option disabled selected value>Please select a country</option>
-    {countries.map((country, index) => (
-      <option value={country}>{country}</option>
-    ))}
-  </select>
+    <select value={selectedCountry} onChange={(e) => setSelectedCountry(e.target.value)}>
+      <option disabled selected value>Please select a country</option>
+      {countries.map((country, index) => (
+        <option value={country}>{country}</option>
+      ))}
+    </select>
   );
 };
 
@@ -18,19 +19,20 @@ const SelectCity = (props) => {
   const { selectedCity, setSelectedCity, cities } = props;
 
   return (
-  <select value={selectedCity} onChange={(e) => setSelectedCity(e.target.value)}>
-    <option disabled selected value>Please select a city</option>
-    {cities.map((city, index) => (
-      <option value={city}>{city}</option>
-    ))}
-  </select>
+    <select value={selectedCity} onChange={(e) => setSelectedCity(e.target.value)}>
+      <option disabled selected value>Please select a city</option>
+      {cities.map((city, index) => (
+        <option value={city}>{city}</option>
+      ))}
+    </select>
   );
 };
 
 export default function SelectCountryAndCity(props) {
   const { countriesAndCities } = props;
-  const [ selectedCountry, setSelectedCountry ] = useState(null);
-  const [ selectedCity, setSelectedCity ] = useState(null);
+  const [selectedCountry, setSelectedCountry] = useState(null);
+  const [selectedCity, setSelectedCity] = useState(null);
+  const [uniqueId, setUniqueId] = useState(0);
 
   const countries = Object.keys(countriesAndCities);
   const cities = countriesAndCities[selectedCountry];
@@ -38,6 +40,9 @@ export default function SelectCountryAndCity(props) {
   function replaceCountry(newCountry) {
     setSelectedCountry(newCountry);
     setSelectedCity(null);
+    setUniqueId(c => c + 1);
+    //setUniqueId((c) => c + 1);
+    //setUniqueId((c) => { return c + 1 });
   }
 
   return (
@@ -50,12 +55,12 @@ export default function SelectCountryAndCity(props) {
       />
 
       {cities &&
-      <SelectCity
-      cities={cities}
-      selectedCity={selectedCity}
-      setSelectedCity={setSelectedCity}
-    />
-    }
+        <SelectCity key={uniqueId}
+          cities={cities}
+          selectedCity={selectedCity}
+          setSelectedCity={setSelectedCity}
+        />
+      }
     </div>
   );
 }
